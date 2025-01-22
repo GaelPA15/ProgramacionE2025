e7cb746dcce4c141753847b63d65deb984fef07b
Gael Velazquez <gaelvela18@gmail.com>
2025-01-21 21:53:00 -0600

    nuevo archivo

---
+++ funcion_matematica.py
@@ -0,0 +1,9 @@
+def sumar(numero1, numero2):
+    
+    resultado = numero1 + numero2
+    return resultado
+
+
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+print(f"La suma de {num1} y {num2} es: {sumar(num1, num2)}")
